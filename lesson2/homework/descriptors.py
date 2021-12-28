0644ef80fae6a90d0d6f91e7ffb569418fd8149c
Nastya Krasun <nastyakrasun@gmail.com>
2021-12-28 20:13:55 +0400

    to lesson2

--- lesson2/homework/descriptors.py
+++ lesson2/homework/descriptors.py
@@ -5,8 +5,9 @@ logger = logging.getLogger('server')
 # Дескриптор для описания порта:
 class Port:
     def __set__(self, instance, value):
-        # instance - <__main__.Server object at 0x000000D582740C50>
-        # value - 7777
+        # check correctness of port value
+        # instance - names of a class (<__main__.Server object at 0x000000D582740C50>)
+        # value - port value (7777)
         if not 1023 < value < 65536:
             logger.critical(
                 f'Попытка запуска сервера с указанием неподходящего порта {value}. Допустимы адреса с 1024 до 65535.')
@@ -18,4 +19,3 @@ class Port:
         # owner - <class '__main__.Server'>
         # name - port
         self.name = name
-
